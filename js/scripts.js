b99e3c8db786e8e5b6522999e85d0685f98190e3
kae2604 <kae2604@ukr.net>
2025-07-06 16:08:53 +0300

    Re-init greenshop project

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$('.owl-carousel').owlCarousel({
+    autoplay:true,
+    autoplayTimeout: 2500,
+    autoplaySpeed: 1000,
+    loop: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        1100:{
+            items:1
+        }
+    }
+
+})
